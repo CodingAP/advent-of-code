412f19a9fc7d869ee48900e06ee809579427b270
CodingAP <apsoutherngaming07@gmail.com>
2021-12-11 01:23:38 -0600

    day 11 2021

--- years/2021/day11/part1.js
+++ years/2021/day11/part1.js
@@ -1,5 +1,47 @@
 const common = require('../../../scripts/common');
 
 module.exports = input => {
-    return 0;
+    let rows = input.split('\n');
+    let grid = common.create2DArray(rows[0].length, rows.length, (x, y) => {
+        return parseInt(rows[y][x]);
+    });
+
+    let flashes = 0;
+    let doFlash = (x, y, grid) => {
+        grid[y][x] = 'FLASHED';
+        for (let j = -1; j <= 1; j++) {
+            for (let k = -1; k <= 1; k++) {
+                if (j == 0 && k == 0) continue;
+                if ((y + j) < 0 || (y + j) >= rows.length || (x + k) < 0 || (x + k) >= rows[0].length) continue;
+                if (grid[y + j][x + k] == 'FLASHED') continue;
+
+                grid[y + j][x + k]++;
+                if (grid[y + j][x + k] > 9) doFlash(x + k, y + j, grid);
+            }
+        }
+    }
+
+    for (let step = 0; step < 100; step++) {
+        let newGrid = common.create2DArray(rows[0].length, rows.length, 0);
+
+        common.forEach2DArray(grid, (value, x, y) => {
+            newGrid[y][x] = value + 1;
+        });
+
+        common.forEach2DArray(newGrid, (value, x, y) => {
+            if (value > 9) doFlash(x, y, newGrid);
+        });
+
+        common.map2DArray(newGrid, (value, x, y) => {
+            if (value == 'FLASHED') {
+                flashes++;
+                return 0;
+            }
+            return value;
+        });
+
+        grid = newGrid;
+    }
+
+    return flashes;
 }
--- years/2021/day11/part2.js
+++ years/2021/day11/part2.js
@@ -1,5 +1,46 @@
 const common = require('../../../scripts/common');
 
 module.exports = input => {
-    return 0;
+    let rows = input.split('\n');
+    let grid = common.create2DArray(rows[0].length, rows.length, (x, y) => {
+        return parseInt(rows[y][x]);
+    });
+
+    let doFlash = (x, y, grid) => {
+        grid[y][x] = 'FLASHED';
+        for (let j = -1; j <= 1; j++) {
+            for (let k = -1; k <= 1; k++) {
+                if (j == 0 && k == 0) continue;
+                if ((y + j) < 0 || (y + j) >= rows.length || (x + k) < 0 || (x + k) >= rows[0].length) continue;
+                if (grid[y + j][x + k] == 'FLASHED') continue;
+
+                grid[y + j][x + k]++;
+                if (grid[y + j][x + k] > 9) doFlash(x + k, y + j, grid);
+            }
+        }
+    }
+
+    let step = 1;
+    while (true) {
+        let newGrid = common.create2DArray(rows[0].length, rows.length, 0);
+
+        common.forEach2DArray(grid, (value, x, y) => {
+            newGrid[y][x] = value + 1;
+        });
+
+        common.forEach2DArray(newGrid, (value, x, y) => {
+            if (value > 9) doFlash(x, y, newGrid);
+        });
+
+        let allFlashed = true;
+        common.map2DArray(newGrid, (value, x, y) => {
+            if (value == 'FLASHED') return 0;
+            allFlashed = false;
+            return value;
+        });
+
+        if (allFlashed) return step;
+        grid = newGrid;
+        step++;
+    }
 }
