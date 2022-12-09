07a4823dcf578875cf9e8f6c446a089e2c554202
CodingAP <apsoutherngaming07@gmail.com>
2022-12-08 19:46:49 -0600

    updated day 8 2022

--- profiles/github/2022/day8/solution.js
+++ profiles/github/2022/day8/solution.js
@@ -1,85 +1,48 @@
 const part1 = async input => {
-    let rows = input.split('\n');
+    let rows = input.split('\n').map(line => line.split('').map(num => parseInt(num)));
     
     let visible = 0;
     for (let y = 0; y < rows.length; y++) {
         for (let x = 0; x < rows[y].length; x++) {
-            if (x == 0 || y == 0 || x == rows[y].length - 1 || y == rows.length - 1) {
-                visible++;
-                continue;
-            }
+            let trees = [
+                new Array(x).fill(0).map((element, index) => rows[y][index]),
+                new Array(y).fill(0).map((element, index) => rows[index][x]),
+                new Array(rows[y].length - x - 1).fill(0).map((element, index) => rows[y][rows[y].length - index - 1]),
+                new Array(rows.length - y - 1).fill(0).map((element, index) => rows[rows.length - index - 1][x])
+            ];
 
-            let isVisible = true;
-            for (let i = 0; i < x; i++) {
-                if (rows[y][i] >= rows[y][x]) isVisible = false;
-            }
-            if (isVisible) {
-                visible++;
-                continue;
-            }
-
-            isVisible = true;
-            for (let i = x + 1; i < rows[y].length; i++) {
-                if (rows[y][i] >= rows[y][x]) isVisible = false;
-            }
-            if (isVisible) {
-                visible++;
-                continue;
-            }
-
-            isVisible = true;
-            for (let i = 0; i < y; i++) {
-                if (rows[i][x] >= rows[y][x]) isVisible = false;
-            }
-            if (isVisible) {
-                visible++;
-                continue;
-            }
-
-            isVisible = true;
-            for (let i = y + 1; i < rows.length; i++) {
-                if (rows[i][x] >= rows[y][x]) isVisible = false;
-            }
-            if (isVisible) {
-                visible++;
-                continue;
-            }
+            if (trees.reduce((acc, row) => {
+                let visible = true;
+                row.forEach(height => {
+                    if (height >= rows[y][x]) visible = false;
+                });
+                if (visible) acc++;
+                return acc;
+            }, 0) > 0) visible++;
         }
     }
     return visible;
 }
 
 const part2 = async input => {
-    let rows = input.split('\n');
+    let rows = input.split('\n').map(line => line.split('').map(num => parseInt(num)));
 
     let highest = -Infinity;
     for (let y = 0; y < rows.length; y++) {
         for (let x = 0; x < rows[y].length; x++) {
-            let left = 0;
-            for (let i = x - 1; i >= 0; i--) {
-                left++;
-                if (rows[y][i] >= rows[y][x]) break;
-            }
+            let trees = [
+                new Array(x).fill(0).map((element, index) => rows[y][index]),
+                new Array(y).fill(0).map((element, index) => rows[index][x]),
+                new Array(rows[y].length - x - 1).fill(0).map((element, index) => rows[y][rows[y].length - index - 1]),
+                new Array(rows.length - y - 1).fill(0).map((element, index) => rows[rows.length - index - 1][x])
+            ].map(array => array.reverse());
 
-            let right = 0;
-            for (let i = x + 1; i < rows[y].length; i++) {
-                right++;
-                if (rows[y][i] >= rows[y][x]) break;
-            }
-
-            let top = 0;
-            for (let i = y - 1; i >= 0; i--) {
-                top++;
-                if (rows[i][x] >= rows[y][x]) break;
-            }
-
-            let bottom = 0;
-            for (let i = y + 1; i < rows.length; i++) {
-                bottom++;
-                if (rows[i][x] >= rows[y][x]) break;
-            }
+            let score = trees.reduce((score, row) => {
+                let distance = row.findIndex(height => height >= rows[y][x]) + 1;
+                if (distance == 0) distance = row.length;
+                return distance * score;
+            }, 1);
             
-            let score = left * right * top * bottom;
             highest = Math.max(score, highest);
         }
     }
